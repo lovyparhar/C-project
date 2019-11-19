67e66023850ed3ca779d97da5344074f1f5256cb
Sukhamjot-Singh <sukhamjot2001@gmail.com>
2019-11-19 15:45:06 +0530

    czvf

---
+++ newattempt/search_by_returndate.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"structs.h"
+void search_by_returndate(int ni, issuer issue_rec[],int d,int m,int y)
+{
+	for(int i = 0 ; i < ni ;i++ )
+	if( d == issue_rec[i].rdate.d && m == issue_rec[i].rdate.m && y == issue_rec[i].rdate.y)
+	{
+		printf("%d %s %s %d %d/%d/%d %d/%d/%d %d\n",issue_rec[i].sn,issue_rec[i].iname,issue_rec[i].iid,issue_rec[i].bid,issue_rec[i].idate.d,issue_rec[i].idate.m,issue_rec[i].idate.y,issue_rec[i].rdate.d,issue_rec[i].rdate.m,issue_rec[i].rdate.y,issue_rec[i].fine); 
+
+	}
+
+}
